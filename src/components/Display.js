4f090f78912658c86490668a697005910f483512
Collus Watson <cklwatsonjr@gmail.com>
2022-08-27 13:49:33 -0400

    'cut down on code'

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,18 +1,12 @@
 import CharacterCard from "./CharacterCard"
 
-function Display({ characters, nations, roles }) {
+function Display({ characters, nations, roles, onCharacterUpdate, onCharacterDelete }) {
 
     return (
         <div className="Display">
             <h1>Display!!</h1>
             {characters.map((character) => {
-                return <CharacterCard key={character.id} character={character} />
-            })}
-            {nations.map((nation)=>{
-                return <h1 key={nation.id}>{nation.nation_name} located in {nation.nation_location}</h1>
-            })}
-            {roles.map((role)=>{
-                return <h1 key={role.id}>{role.role_title} || {role.role_rating}</h1>
+                return <CharacterCard key={character.id} character={character} onCharacterUpdate={onCharacterUpdate} onCharacterDelete={onCharacterDelete} />
             })}
         </div>
     )
