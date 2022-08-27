aa8da6a979d9815875b67e830a02f586c67eb9fc
Collus Watson <cklwatsonjr@gmail.com>
2022-08-27 13:49:05 -0400

    'chage file name, add useStates, delete request'

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,50 @@
-// scf
 
-function CharacterCard({character}){
-    const {character_name, nation_id, role_id} = character
+import { useState } from "react";
+import EditCharacter from "./EditCharacter";
 
-    return(
+function CharacterCard({ character, onCharacterUpdate, onCharacterDelete }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const { id, character_name, nation_id, role_id } = character
+
+    function handleUpdateCharacter(data){
+        setIsEditing(false)
+        onCharacterUpdate(data)
+        console.log('Character card')
+        console.log(data)
+    }
+    function handleDelete() {
+        fetch(`http://localhost:9292/characters/${id}`, {
+            method: "DELETE"
+        });
+        onCharacterDelete(id);
+    }
+
+    return (
         <div>
-            <h2>{character_name}</h2>
-            <h3>{nation_id}:{role_id}</h3>
+            {isEditing ?
+                <EditCharacter 
+                character={character}
+                onUpdateCharacter={handleUpdateCharacter}
+                 />
+                :
+                <div>
+                    <h2>{character_name}</h2>
+                    <h3>{nation_id}:{role_id}</h3>
+                    <button onClick={() => setIsEditing(true)}>
+                        <span role="img" aria-label="edit">
+                            ✏️
+                        </span>
+                    </button>
+                    <button onClick={handleDelete}>
+                        <span role="img" aria-label="delete">
+                            🗑
+                        </span>
+                    </button>
+                </div>
+            }
+
         </div>
     )
 }
