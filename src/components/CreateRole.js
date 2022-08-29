38cdbf289cb84783d065185b086f8b712488004d
Collus Watson <cklwatsonjr@gmail.com>
2022-08-29 19:04:06 -0400

    'create post route for role'

--- src/components/CreateRole.js
+++ src/components/CreateRole.js
@@ -1,4 +1,24 @@
-function CreateRole() {
+import { useState } from "react"
+
+function CreateRole({onRoleCreate}) {
+    const [form, setForm] = useState({ role_name: '', role_location: 'unknown' })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch("http://localhost:9292/roles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        })
+            .then(r => r.json())
+            .then(data => {
+                onRoleCreate(data)
+            })
+            .catch(e => console.log(e))
+    }
+
     return (
         <form onSubmit={null}>
             <div>Create Role!</div>
@@ -6,7 +26,7 @@ function CreateRole() {
             <label>
                 Role Title:
                 <br/>
-                <input type="text" name="role_title" value={null} onChange={null} />
+                <input type="text" name="role_title" onChange={null} />
             </label>
             <br />
             <label>
