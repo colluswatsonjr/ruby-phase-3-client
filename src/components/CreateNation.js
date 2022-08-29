dabf7a1aea0939f482d150db8abf9dd45a2164e9
Collus Watson <cklwatsonjr@gmail.com>
2022-08-29 19:03:43 -0400

    'create post route for nation'

--- src/components/CreateNation.js
+++ src/components/CreateNation.js
@@ -1,14 +1,39 @@
-function CreateNation() {
+import { useState } from "react"
+
+function CreateNation({ onNationCreate }) {
+    const [form, setForm] = useState({ nation_name: '', nation_location: 'unknown' })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch("http://localhost:9292/nations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        })
+            .then(r => r.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(e => console.log(e))
+    }
+
     return (
-        <form onSubmit={null}>
+        <form onSubmit={handleSubmit}>
             <div>Create Nation!</div>
-            <br/>
+            <br />
             <label>
                 Nation Name:
-                <br/>
-                <input type="text" name="nation_name" value={null} onChange={null} />
+                <br />
+                <input type="text" name="nation_name" onChange={(e) => { setForm({ ...form, nation_name: e.target.value }) }} />
             </label>
-            <br/>
+            <select name="nation_location" value={form.nation_location} onChange={(e) => { setForm({ ...form, nation_location: e.target.value }) }}>
+                <option value={1}>Mountains</option>
+                <option value={2}>Canyons</option>
+                <option value={3}>Valleys</option>
+            </select>
+            <br />
             <input type="submit" value="Submit" />
         </form>
     )
