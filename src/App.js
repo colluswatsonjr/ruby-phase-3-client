1a40d3991c31460185811ed4f952590e7483aa9f
Collus Watson <cklwatsonjr@gmail.com>
2022-08-29 16:43:39 -0400

    'update file style'

--- src/App.js
+++ src/App.js
@@ -2,64 +2,65 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
-import Display from './components/Display';
+import DisplayCharacters from './components/DisplayCharacters';
 import CreateCharacter from './components/CreateCharacter';
 import CreateNation from './components/CreateNation';
 import CreateRole from './components/CreateRole';
 
 function App() {
   const [characters, setCharacters] = useState([])
-  const [nations, setNations] = useState([])
-  const [roles, setRoles] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:9292/characters")
       .then((r) => r.json())
       .then((characters) => setCharacters(characters));
   }, []);
-  useEffect(() => {
-    fetch("http://localhost:9292/nations")
-      .then((r) => r.json())
-      .then((nations) => setNations(nations));
-  }, []);
-  useEffect(() => {
-    fetch("http://localhost:9292/roles")
-      .then((r) => r.json())
-      .then((roles) => setRoles(roles));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/nations")
+  //     .then((r) => r.json())
+  //     .then((nations) => setNations(nations));
+  // }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/roles")
+  //     .then((r) => r.json())
+  //     .then((roles) => setRoles(roles));
+  // }, []);
 
-  function handleCharacterUpdate(data){
+
+  function handleCreateCharacter(newCharacter) {
+    setCharacters([...characters, newCharacter])
+  }
+  function handleCharacterDelete(id) {
+    const updateCharacters = characters.filter((character) => character.id !== id);
+    setCharacters(updateCharacters);
+  }
+  function handleCharacterUpdate(data) {
     console.log(data)
-    const updateCharacters = characters.map((character)=>{
-      if(character.id === data.id){
+    const updateCharacters = characters.map((character) => {
+      if (character.id === data.id) {
         return data
-      }else{
+      } else {
         return character
       }
     })
     setCharacters(updateCharacters)
   }
-  function handleCreateCharacter(newCharacter){
-    setCharacters([...characters, newCharacter])
-  }
-  function handleCharacterDelete(id){
-    const updateCharacters = characters.filter((character)=> character.id !== id);
-    setCharacters(updateCharacters);
-  }
-
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={
+    <Routes>
+      <Route path='/' element={
+        <div className='App'>
+          <ul className='Create'>
+            <h1>Create!</h1>
+            <li><CreateCharacter characters={characters} onCreateCharacter={handleCreateCharacter} /></li>
+            <li><CreateNation /></li>
+            <li><CreateRole /></li>
+          </ul>
           <div>
-            <CreateCharacter onCreateCharacter={handleCreateCharacter}/>
-            <CreateNation/>
-            <CreateRole/>
-            <Display characters={characters} nations={nations} roles={roles} onCharacterUpdate={handleCharacterUpdate} onCharacterDelete={handleCharacterDelete} />
+            <DisplayCharacters onCharacterUpdate={handleCharacterUpdate} onCharacterDelete={handleCharacterDelete} />
           </div>
-        } />
-      </Routes>
-    </div>
+        </div>
+      } />
+    </Routes >
   );
 }
 
